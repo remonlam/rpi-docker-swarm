19376d64baf1c93eaf56aec65fa13d9618c83376
Remon Lam <remon.lam@lamnet.nl>
2015-12-02 10:45:36 +0100

    init commit

---
+++ install_docker.sh
@@ -0,0 +1,17 @@
+## Install Docker CentOS 7
+
+# Use docker repo
+tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+yum install docker-engine
+
+service docker start
+
+chkconfig docker on
